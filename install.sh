4e2e5376f28095f57d34fff202561f87f594ad6c
Morgan Jones <mjones@rice.edu>
2015-02-03 13:32:17 -0600

    enhanced prerequisite testing

--- install.sh
+++ install.sh
@@ -4,14 +4,23 @@ SCRIPT=$(readlink -f "$0")
 DIR=$(dirname "$SCRIPT")
 CWD="$(pwd)"
 
+prereq () {
+  command -v "$1" >/dev/null 2>&1 || { echo >&2 "[!!] Install $1 to continue."; exit 1; }
+  type "$1" >/dev/null 2>&1 || { echo >&2 "[!!] Install $1 to continue."; exit 1; }
+  hash "$1" >/dev/null 2>&1 || { echo >&2 "[!!] Install $1 to continue."; exit 1; }
+  echo `which "$1"`
+}
+
+# Get prereqs
 echo "[**] Performing prerequisite checks"
-command -v git >/dev/null 2>&1 || { echo >&2 "[!!] Install git to continue."; exit 1; }
-type git >/dev/null 2>&1 || { echo >&2 "[!!] Install git to continue."; exit 1; }
-hash git >/dev/null 2>&1 || { echo >&2 "[!!] Install git to continue."; exit 1; }
-GIT="$(which git)"
+GIT="$(prereq git)"
+GREP="$(prereq grep)"
+
 echo "[**] You have all prerequisites. Press ENTER to continue."
+echo "[>>] git=$GIT, grep=$GREP"
 read -t 30 || { echo >&2 "[!!] User abort."; exit 1; }
 
+# Grab submodules
 if [ -z "$NO_SUBMODULES" ]; then
   echo "[**] Initializing submodules"
   $GIT submodule update --init --recursive
@@ -19,9 +28,11 @@ if [ -z "$NO_SUBMODULES" ]; then
   $GIT submodule foreach git pull --ff-only origin master
 fi
 
+# Find install files
 cd "$DIR"
-FILES=`git ls-tree --name-only HEAD | grep -e '.*' 2>/dev/null | grep -ve '.git*' 2>/dev/null`
+FILES=`$GIT ls-tree --name-only HEAD | $GREP -e '^\.' 2>/dev/null | $GREP -ve '.git*'`
 
+# Let the user know which commands we're running
 echo "[**] About to perform the following commands:"
 for i in $FILES; do
   SRC="$(realpath $i)"
@@ -32,6 +43,7 @@ done
 echo "[**] Press ENTER to continue or CTRL+C to abort."
 read -t 30 || { echo >&2 "[!!] User abort."; exit 1; }
 
+# Do it
 for i in $FILES; do
   SRC="$(realpath $i)"
   DEST=~/"$i"
@@ -40,5 +52,7 @@ for i in $FILES; do
   ln -s "$SRC" "$DEST" || { echo >&2 "[!!] Error symlinking $SRC to $DEST"; exit 1; }
 done
 cd "$CWD"
+
+# All done
 echo "[**] Done, logout and login to activate the new settings"
 exit 0
